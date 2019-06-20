76201b0a4fe69e0e3efcd4d78a1643b000935e82
Hannah Tuttle <hannah@Hannahs-Air.PK5001Z>
2019-06-20 12:33:56 -0700

    finished droptown toggle

--- components/Dropdown/Dropdown.js
+++ components/Dropdown/Dropdown.js
@@ -3,7 +3,7 @@ class Dropdown {
     
     // Assign this.element to the dropdown element
     this.element = element;
-    console.log('element', element)
+    //console.log('element', element)
     
     // Get the element with the ".dropdown-button" class found in the dropdown element (look at the HTML for context)
     this.button = this.element.querySelector('.dropdown-button');
@@ -18,11 +18,11 @@ class Dropdown {
   toggleContent() {
     
     // Toggle the ".dropdown-hidden" class off and on
-    this.content.classList.remove('dropdown-hidden');
+    this.content.classList.toggle('dropdown-hidden');
   }
 }
 
 
 // Nothing to do here, just study what the code is doing and move on to the Dropdown class
 let dropdowns = document.querySelectorAll('.dropdown').forEach( dropdown => new Dropdown(dropdown));
-console.log('dropdowns', dropdowns)
+//console.log('dropdowns', dropdowns)
